3956477fcb3698517ba1387d409482ff0d2262a9
puxuyang <puxuyang@gome.com.cn>
2022-08-10 19:13:00 +0800

    1.cyclicBarrier

---
+++ demo02/src/main/java/com/pxy/thread/communication/Communication_cyclicBarrier.java
@@ -0,0 +1,13 @@
+package com.pxy.thread.communication;
+
+/**
+ * @author puxy
+ * @version 1.0
+ * @description cyclicBarrier 线程通信
+ * @date 2022/8/10 14:46
+ */
+public class Communication_cyclicBarrier {
+    /**
+     * {@link com.pxy.thread.cyclic_barrier.CyclicBarrierDemo_01}
+     */
+}
--- demo02/src/main/java/com/pxy/thread/cyclic_barrier/CyclicBarrierDemo_01.java
+++ demo02/src/main/java/com/pxy/thread/cyclic_barrier/CyclicBarrierDemo_01.java
@@ -1,6 +1,7 @@
 package com.pxy.thread.cyclic_barrier;
 
 import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CyclicBarrier;
 
 /**
@@ -15,18 +16,42 @@ public class CyclicBarrierDemo_01 {
      * cyclicBarrier:循环屏障
      * 它允许一组线程相互等待,直到达到屏障点(barrier point)时被拦截的线程从才能继续执行,因为它的barrier可以重用,所以叫它循环屏障
      *
+     * 基于{@link java.util.concurrent.locks.ReentrantLock}实现
      *
      */
     public static void main(String[] args) throws Exception {
 
         // test01();
 
-        //
-        CyclicBarrier cyclicBarrier = new CyclicBarrier(10, ()->{});
+        test02();
 
     }
 
+    private static void test02() throws InterruptedException {
+        // CyclicBarrier(int parties, Runnable barrierAction) runnable由最后一个线程执行
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(10, ()->{
+            System.out.printf("线程 [%s] 执行 最终方法\n", Thread.currentThread().getName());
+        });
+
+        for (int i = 0; i < 10; i++) {
+            new Thread(()->{
+                System.out.printf("线程 [%s] 启动 \n", Thread.currentThread().getName());
+                int await;
+                try {
+                    await = cyclicBarrier.await();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } catch (BrokenBarrierException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.printf("线程 [%s] 执行结束 await = [%d] \n", Thread.currentThread().getName(), await);
+            }, String.valueOf(i)).start();
+            Thread.sleep(500);
+        }
+    }
+
     private static void test01() throws InterruptedException {
+        // CyclicBarrier(int parties)
         CyclicBarrier cyclicBarrier = new CyclicBarrier(10);
 
         for (int i = 0; i < 11; i++) {
